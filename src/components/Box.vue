95e7406552bc95a18d92f9054b4152a89da8c29c
cleberfarias <cleber_afd@hotmail.com>
2025-02-28 10:08:35 -0300

    .

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div
-        class="tw-flex tw-items-center tw-justify-between tw-w-full tw-bg-slate-500 tw-p-4 tw-shadow-md tw-rounded-md dark:tw-bg-[#414141] dark:tw-text-white">
+        class="tw-flex tw-items-center tw-justify-between tw-w-full tw-text-white tw-bg-slate-500 tw-p-4 tw-shadow-md tw-rounded-md dark:tw-bg-[#414141] dark:tw-text-white">
         <slot></slot>
 
     </div>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -15,71 +15,61 @@
         @clicado="resetarTarefa"
         icone="fas fa-redo" 
         texto="Resetar" />
-        <select v-model="modoContagem" class="tw-bg-white tw-text-black tw-px-2 tw-rounded">
+        <select v-model="modoContagem" class="tw-bg-white tw-text-black tw-px-2 tw-rounded dark:tw-bg-[#333] dark:tw-text-white">
             <option value="progressivo">Progressivo</option>
             <option value="regressivo">Regressivo</option>
         </select>
         <input v-if="modoContagem !== 'livre'"
              type="number"
              v-model="tempoEmSegundos"
-             class="tw-p-2 tw-border tw-rounded tw-w-16 tw-text-black"
+             class="tw-p-2 tw-border tw-rounded tw-w-16 tw-text-black dark:tw-bg-[#333] dark:tw-text-white"
              placeholder="Segundos" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'; 
-// Importa a função defineComponent do Vue para definir um componente Vue.
 import CronometroComponent from './Cronometro.vue'; 
-// Importa o componente Cronometro de um arquivo local.
 import BotaoComponent from './Botao.vue'; 
-// Importa o componente Botao de um arquivo local.
 
 export default defineComponent({
     name: 'TemporizadorComponent', 
-    // Define o nome do componente como 'TemporizadorComponent'.
     emits: ['aoTeporizadorFinalizado'], 
-    // Declara que o componente emite um evento chamado 'aoTeporizadorFinalizado'.
     components: {
         CronometroComponent, 
-        // Registra o componente Cronometro para uso dentro deste componente.
         BotaoComponent 
-        // Registra o componente Botao para uso dentro deste componente.
     },
     data() {
         return {
             tempoEmSegundos: 0,
-             // Inicializa a variável tempoEmSegundos com 0.
             cronometro: 0, 
-            // Inicializa a variável cronometro com 0.
-            cronometroRodando: false 
-            // Inicializa a variável cronometroRodando com false.
+            cronometroRodando: false,
+            modoContagem: 'progressivo'
         }
     },
     methods: {
         iniciarTarefa() {
             if (!this.cronometroRodando) { 
-                // Verifica se o cronômetro não está rodando.
                 this.cronometroRodando = true; 
-                // Define cronometroRodando como true para indicar que o cronômetro está rodando.
                 this.cronometro = setInterval(() => { 
-                    // Inicia um intervalo que executa a função a cada segundo.
-                    this.tempoEmSegundos++; 
-                    // Incrementa a variável tempoEmSegundos em 1 a cada segundo.
+                    if (this.modoContagem === 'progressivo') {
+                        this.tempoEmSegundos++; 
+                    } else if (this.modoContagem === 'regressivo') {
+                        if (this.tempoEmSegundos > 0) {
+                            this.tempoEmSegundos--; 
+                        } else {
+                            this.pararTarefa();
+                        }
+                    }
                 }, 1000);
             }
         },
         pararTarefa() {
             if (this.cronometroRodando) { 
-                // Verifica se o cronômetro está rodando.
                 clearInterval(this.cronometro); 
-                // Para o intervalo do cronômetro.
                 this.cronometroRodando = false; 
-                // Define cronometroRodando como false para indicar que o cronômetro parou.
                 this.$emit('aoTeporizadorFinalizado', this.tempoEmSegundos); 
-                // Emite o evento 'aoTeporizadorFinalizado' com o valor de tempoEmSegundos.
                 this.tempoEmSegundos = 0; 
-                // Reseta a variável tempoEmSegundos para 0.
             }
         },
         resetarTarefa() {
